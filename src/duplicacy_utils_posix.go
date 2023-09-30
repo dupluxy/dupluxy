3468117fe19a393a63f13020a6c445e17c410cd9
John K. Luebs <jkl@johnluebs.com>
2023-09-29 21:48:47 -0500

    Fix exclude_by_attribute feature on POSIX
    The exclude by attribute function is broken on non-Darwin POSIX: linux and freebsd.
    This is because those xattrs must be prefixed by a legal namespace. The old xattr
    library implicitly appended the user namespace to the xattr, but the current
    official go pkg does not (which is just as well).
    
    Also fix the test to remove the discordant old xattr dependency and provide
    test cases for both darwin and non-darwin POSIX.

--- src/duplicacy_utils_posix.go
+++ src/duplicacy_utils_posix.go
@@ -2,13 +2,12 @@
 // Free for personal use and commercial trial
 // Commercial use requires per-user licenses available from https://duplicacy.com
 
+//go:build freebsd || netbsd || linux || solaris
+// +build freebsd netbsd linux solaris
+
 package duplicacy
 
-import (
-	"strings"
-)
-
 func excludedByAttribute(attributes map[string][]byte) bool {
-	value, ok := attributes["com.apple.metadata:com_apple_backup_excludeItem"]
-	return ok && strings.Contains(string(value), "com.apple.backupd")
+	_, ok := attributes["user.duplicacy_exclude"]
+	return ok
 }
